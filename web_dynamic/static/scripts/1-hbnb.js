1c1106ae47fd6ebe662691da881df84a469061e7
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-04-05 02:44:20 +0400

    Clean up the code

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,34 @@
 /* global $ */
 $(document).ready(function () {
   const amenities = [];
+
   $('input[type=checkbox]').change(function () {
-    if ($(this).get(0).checked) {
-      amenities.push({ [$(this).attr('data-id')]: $(this).attr('data-name') });
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+
+    if (this.checked) {
+      amenities.push({ [amenityId]: amenityName });
     } else {
-      for (let i = 0; i < amenities.length; i++) {
-        if (Object.keys(amenities[i])[0] === $(this).attr('data-id')) {
-          amenities.splice(i, 1);
-          break;
-        }
-      }
+      amenities.splice(
+        amenities.findIndex((amenity) => Object.keys(amenity)[0] === amenityId),
+        1
+      );
     }
-    const am = amenities.map((amenity) => Object.values(amenity)[0]).join(', ');
-    if (amenities.length === 0) {
+
+    updateAmenitiesText();
+  });
+
+  function updateAmenitiesText () {
+    const amenityNames = amenities.map((amenity) => Object.values(amenity)[0]);
+    const text = amenityNames.join(', ');
+
+    if (amenityNames.length === 0) {
       $('.amenities h4').html('&nbsp;');
     } else {
-      $('.amenities h4').text(am.length > 60 ? am.substr(0, 60) + '...' : am);
+      $('.amenities h4').text(
+        text.length > 60 ? `${text.substr(0, 60)}...` : text
+      );
+      $('.amenities h4').css('white-space', 'nowrap');
     }
-  });
+  }
 });
